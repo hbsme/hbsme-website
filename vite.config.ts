9e306041b8dec83436fdb2a9fed4e13859bf2751
Basile <basile@chrm.fr>
2026-03-04 12:51:37 +0000

    chore: allow dev.hbsme.fr host in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  server: {
+    allowedHosts: ['dev.hbsme.fr'],
+  },
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
